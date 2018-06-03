f7209258ae84ca9d1bead2008dec898049d9478a
hoodielive <hoodielive@gmail.com>
2018-06-02 21:19:16 -0400

    expand emitter to include logger

--- events/app.js
+++ events/app.js
@@ -1,17 +1,22 @@
 
 const EventEmitter = require('events'); 
+const logme = require('./logger'); 
+
+// emit -> make a noise or produce something, signaling an event has happened
 
 // Class a container for functions, properties - related methods and properties 
 
 var emitter = new EventEmitter(); 
 
 // Register a Listener
-emitter.on('messageLogged', () => {
-	console.log('Listener called!'); 
+emitter.on('messageLogged', function(arg) {
+	console.log('Listener called!', arg); 
 
 }); 
 
 // Raise an event -> you also have to register a listener
-emitter.emit('messageLogged'); 
+emitter.emit('messageLogged', {id: 1, url: 'http://' }); 
 
-// emit -> make a noise or produce something, signaling an event has happened
+logme('I got your message young man!'); 
+
+
---
+++ events/logger.js
@@ -0,0 +1,7 @@
+
+function log(messageLogged) {
+	console.log(messageLogged); 
+
+}; 
+
+module.exports = log; 
