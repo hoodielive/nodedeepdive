c57ef77440436e693457589047ef665aa1292488
hoodielive <hoodielive@gmail.com>
2018-06-04 21:45:20 -0400

    ver control - dependencies - npm

--- npm-demo/app.js
+++ npm-demo/app.js
@@ -1,7 +1,10 @@
 
 var _ = require('underscore'); 
+var mongoose = require('mongoose'); 
 
 // Assumes this is a Core Module (file or folder) or in node_modules 
 
 var result = _.contains([1,2,3], 2); 
 console.log(result); 
+
+
