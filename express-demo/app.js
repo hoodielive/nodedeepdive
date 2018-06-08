dbd73cb198494c4883420e144bf323696211eb74
hoodielive <hoodielive@gmail.com>
2018-06-07 20:03:13 -0400

    created an api and evoked a 404 spirit

--- express-demo/app.js
+++ express-demo/app.js
@@ -2,20 +2,31 @@ const express = require('express');
 
 const app = express(); 
 
-const port = 3000
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+]; 
 
-# routes => 
+// routes => 
 app.get('/', (req, res) => { 
-
     res.send('hello, aeaon'); 
-
 }); 
 
 app.get('/api/courses', (req, res) => { 
-
-    res.send([1,2,3]); 
+    res.send(courses); 
 }); 
 
-app.listen(port, () => console.log('Listening on port 3000...'));
+app.get('/api/courses/:id', (req, res) => {
+   const course = courses.find(c => c.id === parseInt(req.params.id)); 
+    if (!course) res.status(404).send('The course with the given ID was not found')
+    res.send(course);
+}); 
+
+// PORT ENV 
+
+const port = process.env.PORT || 5000; 
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
