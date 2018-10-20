70d0f6c9b0bd19e719a6a7edf247f506ed9fcece
hoodielive <hoodielive@gmail.com>
2018-10-20 09:13:25 -0400

    removing useless --harmony switch as these features are already implemented

--- filesystems/cat.js
+++ filesystems/cat.js
@@ -1,4 +1,5 @@
-#!/usr/bin/env node --harmony 
+#!/usr/bin/env node
+
 // create a file stream to pipe a file's data to stdout 
 
 require('fs').createReadStream(process.argv[2]).pipe(process.stdout); 
