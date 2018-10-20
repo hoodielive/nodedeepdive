64e2c2ed1706e02e073f86b749f86e9237832527
hoodielive <hoodielive@gmail.com>
2018-10-19 23:07:38 -0400

    spawn process object

--- filesystems/watcher-spawn.js
+++ filesystems/watcher-spawn.js
@@ -1,16 +1,14 @@
 "use strict";
 
-const
-fs = require('fs'),
-spawn = require('child_process'),
-filename = process.argv[2];
+const fs = require('fs'), spawn = require('child_process').spawn, filename = process.argv[2];
 
 if (!filename) {
     throw Error("A file to watch must be specified!"); 
 }
 
 fs.watch(filename, function() {
-    let ls = spawn('ls', ['-lh', filename]);
+  let ls = spawn('ls', ['-lh', filename]); // the obj returned by spawn is a ChildProcess. Its stdin, stdout, and stderr
+  // properties are Streams that can be used to read or write data. 
     ls.stdout.pipe(process.stdout); 
 });
 
