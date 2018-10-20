1d3d07345ab3d7e3223252fc1fe03b9fe759eeeb
hoodielive <hoodielive@gmail.com>
2018-10-20 10:24:01 -0400

    you can transform data using streams; however, I wouldn't trust node-core to do this as there are a lot of updates that happen with node and I do not want to risk breaking my apps by relying on node-core modules

---
+++ transform-data.js
@@ -0,0 +1,4 @@
+filesystems/Streams/transform-data.js|1 col 12 error| 'require' is not defined. (no-undef)
+filesystems/Streams/transform-data.js|2 col 13 error| 'require' is not defined. (no-undef)
+filesystems/Streams/transform-data.js|4 col 21 error| 'process' is not defined. (no-undef)
+filesystems/Streams/transform-data.js|6 col 9 error| 'process' is not defined. (no-undef)
