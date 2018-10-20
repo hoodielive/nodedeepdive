15d38de517bed2ced161303a00bab57e1668aea5
hoodielive <hoodielive@gmail.com>
2018-10-20 14:48:51 -0400

    transform stdin with through2 library

--- filesystems/Streams/through-transform.js
+++ filesystems/Streams/through-transform.js
@@ -1,23 +1,12 @@
-const fs = require("fs"),
-  through = require("through2");
+const Transform = require('stream').Transform
 
-fs.createReadStream(process.argv[2])
-  .pipe(through(write))
-  .pipe(process.stdout);
-
-/**  
- * I am using node-core here but I will prefer readable packages because I do not want to risk Node's updates breaking
- * my apps 
-**/
-
-/**function write(buf, enc, next) {
-  next(null, buf.toString().toUpperCase());
-}
-**/
-
-function toUpper () { 
-  return through(function (buf, enc, next) {
+const toUpper = new Transform({
+  transform: function(buf, enc, next) {
     next(null, buf.toString().toUpperCase()) 
-  });
-};
+  }
+})
+
+process.stdin
+  .pipe(toUpper)
+  .pipe(process.stdout)
 
